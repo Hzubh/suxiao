9dd6a921e5701511301c1809997d75706964076d
Hzubh <2825810455@qq.com>
2023-05-29 10:31:09 +0800

    二级分类-面包屑导航

--- xtx/src/apis/category.js
+++ xtx/src/apis/category.js
@@ -8,3 +8,22 @@ export function getcategoryApi(id) {
         
     })
 }
+
+
+export function getsubcategoryFilterApi(id) {
+    return http({
+        url:'category/sub/filter',
+        params:{
+            id
+        }
+    })
+}
+
+
+export function getsubcategoryApi() {
+    return http({
+        url:'category/goods/temporary',
+        method:'POST',
+        data
+    })
+}
